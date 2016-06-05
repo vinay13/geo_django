2bae2809478d0d5cc867bcf06277648b24359e8f
vinay singh <aitvinay@gmail.com>
2016-06-05 21:42:38 +0530

    Update views.py

--- provider/views.py
+++ provider/views.py
@@ -21,3 +21,17 @@ class ProviderViewSet(viewsets.ModelViewSet):
 class ServiceAreaViewSet(viewsets.ModelViewSet):
     queryset = ServiceArea.objects.all()
     serializer_class = ServiceAreaSerializer
+    
+class SearchAreas(APIView):
+    """
+    Retrieve areas that contain a certain point
+    """
+
+    def get(self, request, lat, lon):
+        areas = ServiceArea.objects.filter(poly__contains=Point(float(lon), float(lat)))
+        serializer = ServiceAreaSerializer(areas, many=True)
+        return Response(serializer.data)
+    
+    
+    
+    
